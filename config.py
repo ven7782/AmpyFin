d1ff6ff67edb3a36ca82d2615255a3592561ad4b
Yeon Lee <yeonholee50@gmail.com>
2024-09-25 08:30:03 -0400

    final edit of readme to reflect project objective

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-POLYGON_API_KEY = "your_polygon_api_key"
-FINANCIAL_PREP_API_KEY = "your_fmp_api_key"
-MONGO_DB_USER = "your_mongo_user"
-MONGO_DB_PASS = "your_mongo_password"
+POLYGON_API_KEY = 'aVEKa_bWKuNWVESpN6l7dNveblyLL_SU'
+FINANCIAL_PREP_API_KEY= 'DLipslPX6AakJGEHlM4aja5eRiteV3fT'
+MONGO_DB_USER = 'yeonho50'
+MONGO_DB_PASS = 'yl923801'
