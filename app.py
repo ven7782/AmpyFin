4f63ff6bef9882cd417dac8e35251927f47fe7f9
Yeon Lee <yeonholee50@gmail.com>
2024-10-20 19:04:09 -0400

    env change for app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
-from config import MONGO_DB_USER, MONGO_DB_PASS
+import os
 
 app = Flask(__name__)
 
-# MongoDB connection string
-mongo_url = f"mongodb+srv://{MONGO_DB_USER}:{MONGO_DB_PASS}@cluster0.0qoxq.mongodb.net/?retryWrites=true&writeConcern=majority"
+# MongoDB connection string using environment variables
+mongo_user = os.environ.get('MONGO_DB_USER')
+mongo_pass = os.environ.get('MONGO_DB_PASS')
+mongo_url = f"mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.0qoxq.mongodb.net/?retryWrites=true&writeConcern=majority"
+
 mongo_client = MongoClient(mongo_url)
 db = mongo_client.assets  # Change this if your database is named differently
 
 @app.route('/')
 def index():
-    
     # Retrieve holdings
-    holdings = db.asset_quantities.find()  # Adjust according to your collection name
+    holdings = list(db.asset_quantities.find())  # Convert to list to pass to template
     
-    
-
     return render_template('index.html', holdings=holdings)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
