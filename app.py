da27499d3bbc48deef138591a06f3cb3aa0eb826
Yeon Lee <yeonholee50@gmail.com>
2024-10-20 19:17:43 -0400

    added auto refresh

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
 
 # MongoDB connection string using environment variables
-mongo_user = os.environ.get('MONGO_DB_USER')
-mongo_pass = os.environ.get('MONGO_DB_PASS')
-mongo_url = f"mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.0qoxq.mongodb.net/?retryWrites=true&writeConcern=majority"
-
+mongo_url = f"mongodb+srv://{os.environ['MONGO_DB_USER']}:{os.environ['MONGO_DB_PASS']}@cluster0.0qoxq.mongodb.net/?retryWrites=true&writeConcern=majority"
 mongo_client = MongoClient(mongo_url)
 db = mongo_client.assets  # Change this if your database is named differently
 
 @app.route('/')
 def index():
-    # Retrieve holdings
-    holdings = list(db.asset_quantities.find())  # Convert to list to pass to template
-    
-    return render_template('index.html', holdings=holdings)
+    return render_template('index.html')
+
+@app.route('/holdings')
+def get_holdings():
+    holdings = list(db.asset_quantities.find())
+    return jsonify(holdings)
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
+    app.run(debug=True)
