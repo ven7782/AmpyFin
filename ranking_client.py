a5b676dc39916535521b9464931389338c4508f1
Yeon Lee <yeonholee50@gmail.com>
2024-10-28 00:56:51 -0400

    ranking stage is only left now. reimplement another more robust ranking v2

--- ranking_client.py
+++ ranking_client.py
@@ -40,12 +40,12 @@ from alpaca.trading.enums import (
     QueryOrderStatus
 )
 from alpaca.common.exceptions import APIError
-import strategies.new_trading_strategies as trading_strategies
+import strategies.trading_strategies_v2 as trading_strategies
 import math
 import yfinance as yf
 import logging
 from collections import Counter
-from trader_client import market_status
+from trading_client import market_status
 from helper_files.client_helper import get_ndaq_tickers
 
 # Connect to MongoDB  
@@ -90,38 +90,14 @@ def main():
             logging.info("Market is open. Waiting for 60 seconds.")
             if not ndaq_tickers:
                 ndaq_tickers = get_ndaq_tickers(mongo_url)  # Fetch tickers using the helper function
-            account = trading_client.get_account()
-
             for ticker in ndaq_tickers:
-                decisions_and_quantities = []
-                buying_power = float(account.cash)
-                portfolio_value = float(account.portfolio_value)
-                cash_to_portfolio_ratio = buying_power / portfolio_value
-
-                try:
-                    historical_data = trading_strategies.get_historical_data(ticker, data_client)
-                    ticker_yahoo = yf.Ticker(ticker)
-                    data = ticker_yahoo.history()
-                    current_price = data['Close'].iloc[-1]
-
-                    asset_info = asset_collection.find_one({'symbol': ticker})
-                    portfolio_qty = asset_info['qty'] if asset_info else 0.0
-
-                    for strategy in [trading_strategies.mean_reversion_strategy, trading_strategies.momentum_strategy,
-                                     trading_strategies.bollinger_bands_strategy, trading_strategies.rsi_strategy, 
-                                     trading_strategies.macd_strategy]:
-                        decision, quantity, _ = strategy(ticker, current_price, historical_data,
-                                                      buying_power, portfolio_qty, portfolio_value)
-                        decisions_and_quantities.append((decision, quantity))
-
+                
+                """
+                insert ranking logic here
+                """
                     
 
-                    """
-                    insert logic here
-                    """
-
-                except Exception as e:
-                    logging.error(f"Error processing {ticker}: {e}")
+                    
 
             time.sleep(60)
 
