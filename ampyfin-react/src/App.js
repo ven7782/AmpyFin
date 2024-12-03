51e109de09f85a72c41dcd6d0659b514aad2ec3e
Yeon Lee <yeonholee50@gmail.com>
2024-12-03 12:22:16 -0500

    added helmet

--- ampyfin-react/src/App.js
+++ ampyfin-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 const API_URL = "https://ampyfin-api-app.onrender.com";
 
